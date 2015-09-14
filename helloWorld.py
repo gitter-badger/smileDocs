0387896e5daf7c4f6aa89ca4d8ca044e83abda5f
beegica <beegjacques@yahoo.com>
2015-09-14 12:03:08 -0400

    First Commit

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+#This is all you need to import! Woot!
+from smile import * 
+
+#This is the line that defines our experiment:
+exp = Experiment()
+
+Label(text="Hello World! Are you ready to SMILE?", duration=5)
+
+exp.run()
